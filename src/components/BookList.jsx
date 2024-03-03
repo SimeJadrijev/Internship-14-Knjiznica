1188774c63a272e6f006d9281f30fa8d9859d944
SimeJadrijev <sime.jadrijev@gmail.com>
2024-03-03 22:14:12 +0100

    Added borrow and return buttons inside the book-card and added the filter input fields

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,4 +1,4 @@
-const BookList = ( {books} ) =>{
+const BookList = ({ books }) => {
 
     // const sortedBooks = books.sort( (a,b) => {
     //     if (a.author !== b.author)
@@ -9,19 +9,125 @@ const BookList = ( {books} ) =>{
     //         a.publicationYear - b.publicationYear;
     // });
 
+//     const [searchQuery, setSearchQuery] = useState("");
+//   const [selectedGenre, setSelectedGenre] = useState("");
+
+//   const filteredBooks = books.filter((book) => {
+//     // Filtriranje po nazivu djela i imenu autora
+//     const titleAndAuthorMatch = book.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+//       book.author.toLowerCase().includes(searchQuery.toLowerCase());
+    
+//     // Filtriranje po žanru
+//     const genreMatch = selectedGenre === "" || book.genre === selectedGenre;
+
+//     return titleAndAuthorMatch && genreMatch;
+
     return (
         <>
-            <div className="book-card">
-                <img src="https://katalog.gkmm.hr/pagesResults/cover.aspx?bibliografskiZapisId=370018450&thumb=0&pozivatelj=rezultati" alt="" />
-                <div className="book-card-right">
-                    <h3 className="id"><span>id:</span> 9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d</h3>
-                    <h3 className="title"><span>naslov:</span> Koko u Parizu</h3>
-                    <h3 className="author"><span>autor:</span> Roko Dimić</h3>
-                    <h3 className="publisher"><span>izdavačka kuća:</span> Udruga Crtani Romani Šou</h3>
-                    <h3 className="publication-year"><span>godina izdavanja:</span> 2023</h3>
-                    <h3 className="genre"><span>žanr:</span> Drama</h3>
-                    <h3 className="number-of-copies"><span>broj:</span> 22</h3>
+
+            <h2 className="section-title">SVE KNJIGE</h2>
+
+            <div className="filters-container">
+                <input
+                    type="text"
+                    placeholder="Pretraži po naslovu ili autoru"
+                    // value={searchQuery}
+                  /*  onChange={(e) => setSearchQuery(e.target.value)} */
+                />
+                <select /* value={selectedGenre} onChange={(e) => setSelectedGenre(e.target.value)} */>
+                    <option value="">Filtriraj po žanru</option>
+                    <option value="roman">Roman</option>
+                    <option value="poezija">Poezija</option>
+                    <option value="drama">Drama</option>
+                    <option value="fantazija">Fantazija</option>
+                    <option value="znanstvena fantastika">Znanstvena fantastika</option>
+                    <option value="biografija">Biografija</option>
+                </select>
+                <button>Filtriraj</button>
+            </div>
+
+            <div className="books-container">
+
+                <div className="book-card">
+                    <img src="https://katalog.gkmm.hr/pagesResults/cover.aspx?bibliografskiZapisId=370018450&thumb=0&pozivatelj=rezultati" alt="" />
+                    <div className="book-card-right">
+                        <div className="book-card-top">
+                            <h3 className="id"><span>id:</span> 9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d</h3>
+                            <h3 className="title"><span>naslov:</span> Koko u Parizu</h3>
+                            <h3 className="author"><span>autor:</span> Roko Dimić</h3>
+                            <h3 className="publisher"><span>izdavačka kuća:</span> Udruga Crtani Romani Šou</h3>
+                            <h3 className="publication-year"><span>godina izdavanja:</span> 2023</h3>
+                            <h3 className="genre"><span>žanr:</span> Drama</h3>
+                            <h3 className="number-of-copies"><span>broj:</span> 22</h3>
+                        </div>
+
+                        <div className="book-card-bottom">
+                            <button id="borrow-book-btn">vrati</button>
+                            <button id="return-book-btn">posudi</button>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="book-card">
+                    <img src="https://katalog.gkmm.hr/pagesResults/cover.aspx?bibliografskiZapisId=370018450&thumb=0&pozivatelj=rezultati" alt="" />
+                    <div className="book-card-right">
+                        <div className="book-card-top">
+                            <h3 className="id"><span>id:</span> 9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d</h3>
+                            <h3 className="title"><span>naslov:</span> Koko u Parizu</h3>
+                            <h3 className="author"><span>autor:</span> Roko Dimić</h3>
+                            <h3 className="publisher"><span>izdavačka kuća:</span> Udruga Crtani Romani Šou</h3>
+                            <h3 className="publication-year"><span>godina izdavanja:</span> 2023</h3>
+                            <h3 className="genre"><span>žanr:</span> Drama</h3>
+                            <h3 className="number-of-copies"><span>broj:</span> 22</h3>
+                        </div>
+
+                        <div className="book-card-bottom">
+                            <button id="borrow-book-btn">vrati</button>
+                            <button id="return-book-btn">posudi</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="book-card">
+                    <img src="https://katalog.gkmm.hr/pagesResults/cover.aspx?bibliografskiZapisId=370018450&thumb=0&pozivatelj=rezultati" alt="" />
+                    <div className="book-card-right">
+                        <div className="book-card-top">
+                            <h3 className="id"><span>id:</span> 9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d</h3>
+                            <h3 className="title"><span>naslov:</span> Koko u Parizu</h3>
+                            <h3 className="author"><span>autor:</span> Roko Dimić</h3>
+                            <h3 className="publisher"><span>izdavačka kuća:</span> Udruga Crtani Romani Šou</h3>
+                            <h3 className="publication-year"><span>godina izdavanja:</span> 2023</h3>
+                            <h3 className="genre"><span>žanr:</span> Drama</h3>
+                            <h3 className="number-of-copies"><span>broj:</span> 22</h3>
+                        </div>
+
+                        <div className="book-card-bottom">
+                            <button id="borrow-book-btn">vrati</button>
+                            <button id="return-book-btn">posudi</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="book-card">
+                    <img src="https://katalog.gkmm.hr/pagesResults/cover.aspx?bibliografskiZapisId=370018450&thumb=0&pozivatelj=rezultati" alt="" />
+                    <div className="book-card-right">
+                        <div className="book-card-top">
+                            <h3 className="id"><span>id:</span> 9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d</h3>
+                            <h3 className="title"><span>naslov:</span> Koko u Parizu</h3>
+                            <h3 className="author"><span>autor:</span> Roko Dimić</h3>
+                            <h3 className="publisher"><span>izdavačka kuća:</span> Udruga Crtani Romani Šou</h3>
+                            <h3 className="publication-year"><span>godina izdavanja:</span> 2023</h3>
+                            <h3 className="genre"><span>žanr:</span> Drama</h3>
+                            <h3 className="number-of-copies"><span>broj:</span> 22</h3>
+                        </div>
+
+                        <div className="book-card-bottom">
+                            <button id="borrow-book-btn">vrati</button>
+                            <button id="return-book-btn">posudi</button>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </>
     )
